0aa1e473dd84845448e431b238d8538ce7184593
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-02-20 23:18:57 -0800

    Remove bad endpoint

--- web.py
+++ web.py
@@ -5,7 +5,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 auth = (os.environ["FLICKR_KEY"], os.environ["FLICKR_SECRET"]) 
 
-
 @app.route("/")
 def api_root(in_url):
     return jsonify({
@@ -13,13 +12,6 @@ def api_root(in_url):
             "/photos",
             ]
         })
-
-@app.route("/photos")
-def index():
-    return render_template("index.html",
-                source="http://responsivewebdesign.com/robot/",
-                text="http://responsivewebdesign.com/robot/")
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
